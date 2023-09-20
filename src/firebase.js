ad006eb230c99866142ea01edce813f622587de1
Aurelius <89624318+Aurelius77@users.noreply.github.com>
2023-09-20 14:07:01 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBHUsUnkjB8yyIrzNz2-o4g4o4WiIK_as0",
-  authDomain: "gallery-app-a38d2.firebaseapp.com",
-  projectId: "gallery-app-a38d2",
-  storageBucket: "gallery-app-a38d2.appspot.com",
-  messagingSenderId: "653895683136",
-  appId: "1:653895683136:web:d1824278ea43f9395123a5",
-  measurementId: "G-SVB34TCH8T"
+  apiKey: "Your ApI key",
+  authDomain: "your Auth domain",
+  projectId: "your project id",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
